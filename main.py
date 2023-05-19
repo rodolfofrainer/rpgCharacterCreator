c2f0445f32abb46a8537a6aa298e0a34c1b66366
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-19 13:15:11 +0100

    create knight class

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+from random import randint
+from faker import Faker
+
+faker = Faker()
+
+
 class BaseCharacter:
-    def __init__(self, name, level, stats):
+    def __init__(self, name, level):
         self.name = name
         self.level = level
-        self.stats = stats
 
     def __str__(self):
-        return f"{self.name} is level {self.level} with {self.stats} stats."
+        return f"{self.name} is level {self.level}."
 
 
-character1 = BaseCharacter("Bob", 1, {"str": 10, "dex": 10, "int": 10})
-print(character1)
+class KnightCharacter(BaseCharacter):
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+        self.attack = 7 * self.level
+        self.health = 30 * self.level
+
+    def __str__(self):
+        return f"{self.name} is level {self.level} with {self.attack} attack and {self.health} health."
+
+
+for index, obj in enumerate(range(10)):
+    char = KnightCharacter(faker.name(), randint(1, 10))
+    print(f'{index+1}: {char}')
