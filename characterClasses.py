6aa5e6f2d9a8f4cf24272a663c08473282d7a11f
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-20 20:21:32 +0100

    improved KnightCharacter class

--- characterClasses.py
+++ characterClasses.py
@@ -8,11 +8,42 @@ class BaseCharacter:
 
 
 class KnightCharacter(BaseCharacter):
-    def __init__(self, name, level):
+    def __init__(self, name, race, alignment, experience_points,
+                 stats={
+                     "Strength": 10,
+                     "Dexterity": 10,
+                     "Constitution": 10,
+                     "Intelligence": 10,
+                     "Wisdom": 10,
+                     "Charisma": 10
+                 }):
         self.name = name
-        self.level = level
-        self.attack = 7 * self.level
-        self.health = 30 * self.level
+        self.race = race
+        self.alignment = alignment
+        self.experience_points = experience_points
+        self.stats = stats
 
     def __str__(self):
-        return f"{self.name} is level {self.level} with {self.attack} attack and {self.health} health."
+        return f"{self.name} with the stats {self.stats}"
+
+    def calculate_level(self):
+        level = 1
+        experience_points = self.experience_points
+        level_thresholds = [
+            0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000,
+            85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000,
+            305000, 355000
+        ]
+        for i, threshold in enumerate(level_thresholds):
+            if experience_points >= threshold:
+                level = i + 1
+        return level
+
+    def calculate_attack(self):
+        pass
+
+    def calculate_health(self):
+        pass
+
+    def calculate_defense(self):
+        pass
