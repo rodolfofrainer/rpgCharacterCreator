9e963d78dd22291d54892cfcd42198224eb7950e
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-23 14:01:00 +0100

    create user CLI

--- main.py
+++ main.py
@@ -3,8 +3,37 @@ import os
 from pathlib import Path
 from random import randint
 from characterClasses import KnightCharacter
-from faker import Faker
-import json
+from stats_allocation import Stats
+
+
+def createCharacter():
+    character_name = input("What is your character's name? ")
+    race_options = ["Human", "Elf", "Dwarf",
+                    "Halfling", "Gnome", "Half-Elf", "Half-Orc"]
+    for i, race in enumerate(race_options):
+        print(f"{i + 1}. {race}")
+    character_race = race_options[int(input()) - 1]
+
+    alignment_options = ["Lawful Good", "Neutral Good",
+                         "Chaotic Good", "Lawful Neutral",
+                         "True Neutral", "Chaotic Neutral",
+                         "Lawful Evil", "Neutral Evil",
+                         "Chaotic Evil"]
+    for i, alignment in enumerate(alignment_options):
+        print(f"{i + 1}. {alignment}")
+    character_alignment = alignment_options[int(input()) - 1]
+
+    character_experience = 0
+    character_stats = Stats()
+    character_stats.stats_allocation()
+
+    # Insert character data into the database
+    insert_query = "INSERT INTO characters (name, race, alignment, experience_points, stats) VALUES (?, ?, ?, ?, ?)"
+    values = (character_name, character_race, character_alignment,
+              character_experience, str(character_stats.stats_return()))
+    cursor.execute(insert_query, values)
+    print("Character created and saved.")
+
 
 # Create database if it doesn't exist
 if not os.path.exists("characters.db"):
@@ -18,33 +47,12 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS characters (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, race TEXT, alignment TEXT, experience_points INTEGER, stats TEXT)"
 cursor.execute(create_table)
 
-# Populate data
-
-
-def populate_data(number_of_characters=100):
-    faker = Faker()
-    for _ in range(100):
-        name = faker.name()
-        race = faker.word()
-        alignment = faker.word()
-        experience_points = faker.random_int(min=0, max=100)
-        stats = {
-            "Strength": faker.random_int(min=1, max=20),
-            "Dexterity": faker.random_int(min=1, max=20),
-            "Constitution": faker.random_int(min=1, max=20),
-            "Intelligence": faker.random_int(min=1, max=20),
-            "Wisdom": faker.random_int(min=1, max=20),
-            "Charisma": faker.random_int(min=1, max=20)
-        }
-
-        insert_query = "INSERT INTO characters (name, race, alignment, experience_points, stats) VALUES (?, ?, ?, ?, ?)"
-        cursor.execute(insert_query, (name, race, alignment,
-                       experience_points, json.dumps(stats)))
-
-
-# populate_data(100)
-
+# Create character
+createCharacter()
 
 # Commit changes and close connection
 connection.commit()
 connection.close()
+
+# populate database with 100 entries
+# populate_data(100)
