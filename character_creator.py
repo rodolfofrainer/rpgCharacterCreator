59b71ce86e3897a593668dc3b0043dde3029e62e
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-24 14:53:48 +0100

    create creator class

---
+++ character_creator.py
@@ -0,0 +1,33 @@
+from stats_allocation import Stats
+
+
+class CharacterCreator():
+    def __init__(self, insert_query=None, values=None):
+        self.insert_query = insert_query
+        self.values = values
+
+    def createCharacter(self):
+        character_name = input("What is your character's name? ")
+        race_options = ["Human", "Elf", "Dwarf",
+                        "Halfling", "Gnome", "Half-Elf", "Half-Orc"]
+        for i, race in enumerate(race_options):
+            print(f"{i + 1}. {race}")
+        character_race = race_options[int(input()) - 1]
+
+        alignment_options = ["Lawful Good", "Neutral Good",
+                             "Chaotic Good", "Lawful Neutral",
+                             "True Neutral", "Chaotic Neutral",
+                             "Lawful Evil", "Neutral Evil",
+                             "Chaotic Evil"]
+        for i, alignment in enumerate(alignment_options):
+            print(f"{i + 1}. {alignment}")
+        character_alignment = alignment_options[int(input()) - 1]
+
+        character_experience = 0
+        character_stats = Stats()
+        character_stats.stats_allocation()
+
+        # Insert character data into the database
+        self.insert_query = "INSERT INTO characters (name, race, alignment, experience_points, stats) VALUES (?, ?, ?, ?, ?)"
+        self.values = (character_name, character_race, character_alignment,
+                       character_experience, str(character_stats.stats_return()))
--- main.py
+++ main.py
@@ -3,36 +3,7 @@ import os
 from pathlib import Path
 from random import randint
 from characterClasses import KnightCharacter
-from stats_allocation import Stats
-
-
-def createCharacter():
-    character_name = input("What is your character's name? ")
-    race_options = ["Human", "Elf", "Dwarf",
-                    "Halfling", "Gnome", "Half-Elf", "Half-Orc"]
-    for i, race in enumerate(race_options):
-        print(f"{i + 1}. {race}")
-    character_race = race_options[int(input()) - 1]
-
-    alignment_options = ["Lawful Good", "Neutral Good",
-                         "Chaotic Good", "Lawful Neutral",
-                         "True Neutral", "Chaotic Neutral",
-                         "Lawful Evil", "Neutral Evil",
-                         "Chaotic Evil"]
-    for i, alignment in enumerate(alignment_options):
-        print(f"{i + 1}. {alignment}")
-    character_alignment = alignment_options[int(input()) - 1]
-
-    character_experience = 0
-    character_stats = Stats()
-    character_stats.stats_allocation()
-
-    # Insert character data into the database
-    insert_query = "INSERT INTO characters (name, race, alignment, experience_points, stats) VALUES (?, ?, ?, ?, ?)"
-    values = (character_name, character_race, character_alignment,
-              character_experience, str(character_stats.stats_return()))
-    cursor.execute(insert_query, values)
-    print("Character created and saved.")
+from character_creator import CharacterCreator
 
 
 # Create database if it doesn't exist
@@ -48,11 +19,16 @@ create_table = "CREATE TABLE IF NOT EXISTS characters (id INTEGER PRIMARY KEY AU
 cursor.execute(create_table)
 
 # Create character
-createCharacter()
+createCharacter = CharacterCreator()
+createCharacter.createCharacter()
+cursor.execute(createCharacter.insert_query, createCharacter.values)
+print("Character created and saved.")
+
 
 # Commit changes and close connection
 connection.commit()
 connection.close()
+
 
 # populate database with 100 entries
 # populate_data(100)
